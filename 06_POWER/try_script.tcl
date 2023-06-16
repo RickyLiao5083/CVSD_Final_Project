5f2ee3c5c5824106828c8fa1bbd22b1b6a053b84
RickyLiao5083 <92777818+RickyLiao5083@users.noreply.github.com>
2023-06-16 12:13:44 +0800

    Latest version (Debugged with TA's tb). Cycle=21ns

--- 06_POWER/try_script.tcl
+++ 06_POWER/try_script.tcl
@@ -7,7 +7,7 @@ current_design ml_demodulator
 link
 
 # ===== modified to your max clock freq ===== #
-create_clock -period 22.0 [get_ports i_clk]
+create_clock -period 21.0 [get_ports i_clk]
 set_propagated_clock      [get_clock i_clk]
 # ===== active window ===== #
 read_fsdb  -strip_path testbed/u_ml_demodulator ../05_POST/ml_demodulator.fsdb
